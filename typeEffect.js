3b0489a5201f1949863804fef7ea7c6ea2851da1
Kevin <email@mail.com>
2019-08-03 18:40:06 +0800

    modify type

--- typeEffect.js
+++ typeEffect.js
@@ -2,3 +2,22 @@ var work = ["全端工程師", "程式設計師", "app設計師"];
 var index = 0;
 var speed = 300;
 var className = ".type";
+var count = 0;
+function type() {
+    console.log('type');
+    var t = document.querySelector(className);
+    var text = work[index].slice(0, count++);
+    t.innerHTML = text;
+
+    if (count === work[index].length+1) {
+        index++;
+        count = 0;
+    }
+
+    if (index === work.length) {
+        index = 0;
+    }
+    setTimeout(type, speed);
+}
+
+document.addEventListener("DOMContentLoaded", type());
